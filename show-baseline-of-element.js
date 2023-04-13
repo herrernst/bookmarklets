3589ab837f81b93c4c0f29f2559ad1b78c6838b0
herrernst <herr.ernst@gmail.com>
2023-04-13 17:09:40 +0200

    Create show-baseline-of-element.js

---
+++ show-baseline-of-element.js
@@ -0,0 +1,11 @@
+         &::before {
+           display: inline-block;
+           content: ""; /* "\00a0" */
+           position: relative;
+           width: 1px;
+           height: 1px;
+           margin-right: -1px;
+           background: rgba(0, 0, 255, 0.5);
+           vertical-align: baseline;
+           transform: scaleX(10000000%);
+         }
